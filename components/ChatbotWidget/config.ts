4daf446fa80c05db5a9a73e7d7f66a80a0818239
Selen Şar <sr.selenn@gmail.com>
2023-11-22 22:30:18 +0300

    chatbot widget button added

--- components/ChatbotWidget/config.ts
+++
@@ -1,9 +0,0 @@
-// Config starter code
-import { createChatBotMessage } from "react-chatbot-kit";
-
-const config = {
-  botName: "Log Buster",
-  initialMessages: [createChatBotMessage(`Hello world`, { widget: "options" })],
-};
-
-export default config;
