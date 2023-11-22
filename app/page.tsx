1be2f6a4cfd446701628537e57c5f82c136e105f
Selen Şar <sr.selenn@gmail.com>
2023-11-22 18:39:40 +0300

    chatbot widget added

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,23 @@
+"use client";
+
+import { useState } from "react";
+import { ChatbotWidget } from "../components/ChatbotWidget/ChatbotWidget";
 import styles from "./page.module.css";
 
 export default function Home() {
+  const [isChatbotOpen, setIsChatbotOpen] = useState(false);
+
+  const toggleChatbot = () => {
+    setIsChatbotOpen((isChatbotOpen) => !isChatbotOpen);
+  };
   return (
     <main className={styles.main}>
       <div className={styles.center}></div>
+
+      <button className={styles.button} onClick={toggleChatbot}>
+        {isChatbotOpen ? "Close" : "Open"} Chatbot
+      </button>
+      {isChatbotOpen && <ChatbotWidget toggleChatbot={toggleChatbot} />}
     </main>
   );
 }
