9bd4138639f23512540508a584ed159b5215bea7
sbaltinsoy <serhatburakaltinsoy@gmail.com>
2023-11-22 22:52:13 +0300

    feat: added toastify and mui libraries

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,9 @@
 import { useState } from "react";
 import { ChatbotWidget } from "../components/ChatbotWidget/ChatbotWidget";
 import styles from "./page.module.css";
+import { ToastContainer } from "react-toastify";
+import { StyledEngineProvider } from "@mui/material/styles";
+import "react-toastify/ReactToastify.min.css";
 
 export default function Home() {
   const [isChatbotOpen, setIsChatbotOpen] = useState(false);
@@ -11,13 +14,15 @@ export default function Home() {
     setIsChatbotOpen((isChatbotOpen) => !isChatbotOpen);
   };
   return (
-    <main className={styles.main}>
-      <div className={styles.center}></div>
-
-      <button className={styles.button} onClick={toggleChatbot}>
-        {isChatbotOpen ? "Close" : "Open"} Chatbot
-      </button>
-      {isChatbotOpen && <ChatbotWidget toggleChatbot={toggleChatbot} />}
-    </main>
+    <StyledEngineProvider injectFirst>
+      <main className={styles.main}>
+        <div className={styles.center}></div>
+        <button className={styles.button} onClick={toggleChatbot}>
+          {isChatbotOpen ? "Close" : "Open"} Chatbot
+        </button>
+        {isChatbotOpen && <ChatbotWidget toggleChatbot={toggleChatbot} />}
+      </main>
+      <ToastContainer position="bottom-right" newestOnTop />
+    </StyledEngineProvider>
   );
 }
